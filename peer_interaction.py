8388eb6a562c55e616ffbfc611d801d45b7cc2f7
Cyeciad <loginmish@gmail.com>
2024-08-21 14:40:44 +0500

    task with rare segments complete

--- peer_interaction.py
+++ peer_interaction.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import heapq
 from segment_downloader import SegmentDownloader
 from pubsub import pub
 
@@ -10,7 +11,8 @@ class PeerInteraction:
     def __init__(self, torrent):
         self.peers = []
         self.active_peers = 0
-        self.available_segments = [[0, []] for _ in range(torrent.total_segments)]
+        self.available_segments = [(0, [], False) for _ in range(torrent.total_segments)]
+        self.segment_heap = [(0, i) for i in range(torrent.total_segments)]
         self.torrent = torrent
         self.is_active = True
 
@@ -33,13 +35,25 @@ class PeerInteraction:
             if peer.bitfield[i] == 1:
                 self.available_segments[i][0] += 1
                 self.available_segments[i][1].append(peer)
+                heapq.heappush(self.segment_heap, (self.available_segments[i][0], i))
         self.active_peers += 1
 
-    def remove_peer(self, peer):
+    def get_most_rare_segment(self):
+        count, rarest_index = heapq.heappop(self.segment_heap)
+        # Добавить file_writer и torrent_statistics
+        return SegmentDownloader(segment_id=rarest_index, torrent_data=self.torrent,
+                                 peers=self.available_segments[rarest_index][1][0] if count == 1 else
+                                 self.available_segments[rarest_index][1][:2])
+
+    async def block_peer(self, peer):
         if peer in self.peers:
-            peer.close()
             self.peers.remove(peer)
 
+            self.remove_peer_from_available_segments(peer)
+
+            await peer.close()
+
+    def remove_peer_from_available_segments(self, peer):
         flag = False
         for i in range(len(self.available_segments)):
             if peer in self.available_segments[i][1]:
@@ -54,4 +68,3 @@ class PeerInteraction:
             if peer.peer_choked is False:
                 return True
         return False
-
